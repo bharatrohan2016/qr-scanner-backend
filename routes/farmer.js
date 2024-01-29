52e507de15d4faca55121558493b654ee82945fc
Sanchit <tech@bharatrohan.in>
2024-01-29 12:24:58 +0530

    First Commit

--- routes/farmer.js
+++ routes/farmer.js
@@ -3,7 +3,7 @@ const farmerController = require('../controllers/farmer')
 const router = express.Router()
 const protect = require('../middlewares/authMiddleware')
 
-router.get('/get-farmer', protect ,farmerController.getfarmers)
-router.get('/get-farmer/:id', protect, farmerController.getsinglefarmer)
+router.get('/get-farmer' ,farmerController.getfarmers)
+router.get('/get-farmer/:id', farmerController.getsinglefarmer)
 
 module.exports = router
